fd5217320c439eae64032cd90a1d71b84364ab7c
Yosi Setiawan <yosisetiawan02@gmail.com>
2019-09-11 17:47:05 +0700

    deploy apk

--- src/components/menu/Food.js
+++ src/components/menu/Food.js
@@ -62,7 +62,7 @@ class Food extends Component {
                   <Card style={styles.card}>
                     <Image
                       style={styles.images}
-                      source={{uri: `${'http://192.168.0.16:3000/static/uploads/' + item.images}`}}
+                      source={{uri: `${API_GET_IMAGES + item.images}`}}
                     />
                     <CardItem>
                       <Col>
